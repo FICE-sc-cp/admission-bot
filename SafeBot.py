bdd8d7112e45ac7e445c33ee0bd69ee5d6ac3187
Kulunchick <nikolikrasovski@gmail.com>
2023-07-29 19:40:29 +0300

    FIX: message_thread_id

--- SafeBot.py
+++ SafeBot.py
@@ -1,7 +1,9 @@
 import asyncio
 import logging
+import typing
 
-from aiogram import Bot
+from aiogram import Bot, types
+from aiogram.types import base
 from aiogram.utils import exceptions
 
 from timer import Timer
@@ -25,13 +27,15 @@ class SafeBot(Bot):
         self._scheduler = Timer(1 / self._reqs_per_second, event_scheduler, infinite=True, immediate=True)
         super().__init__(token)
 
-    async def edit_message_text(self, text,
-                                chat_id=None,
-                                message_id=None,
-                                inline_message_id=None,
-                                parse_mode=None,
-                                disable_web_page_preview=None,
-                                reply_markup=None):
+    async def edit_message_text(self,
+                                text: base.String,
+                                chat_id: typing.Union[base.Integer, base.String, None] = None,
+                                message_id: typing.Optional[base.Integer] = None,
+                                inline_message_id: typing.Optional[base.String] = None,
+                                parse_mode: typing.Optional[base.String] = None,
+                                entities: typing.Optional[typing.List[types.MessageEntity]] = None,
+                                disable_web_page_preview: typing.Optional[base.Boolean] = None,
+                                reply_markup: typing.Union[types.InlineKeyboardMarkup, None] = None):
         self._events.append(super(SafeBot, self).edit_message_text(text,
                                                                    chat_id,
                                                                    message_id,
@@ -40,16 +44,30 @@ class SafeBot(Bot):
                                                                    disable_web_page_preview,
                                                                    reply_markup))
 
-    async def send_message(self, chat_id, text, parse_mode=None, disable_web_page_preview=None,
-                           disable_notification=None, reply_to_message_id=None, reply_markup=None):
-        self._events.append(self._send_message(chat_id, text, parse_mode, disable_web_page_preview,
+    async def send_message(self,
+                           chat_id: typing.Union[base.Integer, base.String],
+                           text: base.String,
+                           parse_mode: typing.Optional[base.String] = None,
+                           entities: typing.Optional[typing.List[types.MessageEntity]] = None,
+                           disable_web_page_preview: typing.Optional[base.Boolean] = None,
+                           message_thread_id: typing.Optional[base.Integer] = None,
+                           disable_notification: typing.Optional[base.Boolean] = None,
+                           protect_content: typing.Optional[base.Boolean] = None,
+                           reply_to_message_id: typing.Optional[base.Integer] = None,
+                           allow_sending_without_reply: typing.Optional[base.Boolean] = None,
+                           reply_markup: typing.Union[types.InlineKeyboardMarkup,
+                           types.ReplyKeyboardMarkup,
+                           types.ReplyKeyboardRemove,
+                           types.ForceReply, None] = None
+                           ):
+        self._events.append(self._send_message(chat_id, text, parse_mode, disable_web_page_preview, message_thread_id,
                                                disable_notification, reply_to_message_id, reply_markup))
 
-    async def _send_message(self, chat_id, text, parse_mode=None, disable_web_page_preview=None,
+    async def _send_message(self, chat_id, text, parse_mode=None, disable_web_page_preview=None, message_thread_id=None,
                             disable_notification=None, reply_to_message_id=None, reply_markup=None):
         log = logging.getLogger('bot')
         try:
-            await super(SafeBot, self).send_message(chat_id, text, parse_mode, disable_web_page_preview,
+            await super(SafeBot, self).send_message(chat_id, text, parse_mode, disable_web_page_preview, message_thread_id,
                                                     disable_notification, reply_to_message_id, reply_markup)
         except exceptions.BotBlocked:
             log.error(f"Target [ID:{chat_id}]: blocked by user")
@@ -58,7 +76,7 @@ class SafeBot(Bot):
         except exceptions.RetryAfter as e:
             log.error(f"Target [ID:{chat_id}]: Flood limit is exceeded. Sleep {e.timeout} seconds.")
             await asyncio.sleep(e.timeout)
-            return await self.send_message(chat_id, text, parse_mode, disable_web_page_preview,
+            return await self.send_message(chat_id, text, parse_mode, disable_web_page_preview, message_thread_id,
                                            disable_notification, reply_to_message_id, reply_markup)  # Recursive call
         except exceptions.UserDeactivated:
             log.error(f"Target [ID:{chat_id}]: user is deactivated")
