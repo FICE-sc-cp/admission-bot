24a116e9f183d20f3362e04ad26a5419bb345a43
Kulunchick <nikolikrasovski@gmail.com>
2023-08-05 10:10:43 +0300

    FIX: content type

--- app/bot/admission_api/queue.py
+++ app/bot/admission_api/queue.py
@@ -8,12 +8,12 @@ class QueueAPI(BaseAPI):
 
     async def add_user_to_queue(self, queue_id: Union[str, int], uid: Union[str, int]):
         async with self._session.post(f"{self.path}/{queue_id}/users", json={"id": str(uid)}) as response:
-            return await response.json()
+            return await response.json(content_type=None)
 
     async def remove_user_from_queue(self, queue_id: Union[str, int], uid: Union[str, int]):
         async with self._session.delete(f'{self.path}/{queue_id}/users/{uid}') as response:
-            return await response.json()
+            return await response.json(content_type=None)
 
     async def list_queues(self):
         async with self._session.get(f'{self.path}/') as response:
-            return await response.json()
+            return await response.json(content_type=None)
--- app/bot/admission_api/users.py
+++ app/bot/admission_api/users.py
@@ -9,9 +9,9 @@ class UserAPI(BaseAPI):
 
     async def get_user_info(self, uid: Union[str, int]):
         async with self._session.get(f'{self.path}/{uid}') as response:
-            return await response.json()
+            return await response.json(content_type=None)
 
     async def register_user(self, user: RegisterUser):
         async with self._session.post(f'{self.path}/',
                                       json=user.model_dump(by_alias=True, exclude_none=True)) as response:
-            return await response.json()
+            return await response.json(content_type=None)
