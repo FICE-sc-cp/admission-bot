1a43ef340f60ba5095d785f8210fe8b67067bde3
Kulunchick <nikolikrasovski@gmail.com>
2023-08-05 22:55:55 +0300

    UPD: set default

--- app/messages/api.py
+++ app/messages/api.py
@@ -1,11 +1,11 @@
 from app.messages.environment import environment
 
 CONTRACT_INFO = environment.from_string("""
-<b>ПІБ:</b> {{ contract.last_name }} {{ contract.first_name }} {{ contract.middle_name }}
-<b>Спеціальність:</b> {{ contract.speciality }}
-<b>Номер контракту:</b> {{ contract.contract_number }}
-<b>Конкурсний бал:</b> {{ contract.competitive_point }}
-<b>Дата:</b> {{ contract.date }}
+<b>ПІБ:</b> {{ contract.last_name }} {{ contract.first_name }} {{ contract.middle_name|default('', true) }}
+<b>Спеціальність:</b> {{ contract.speciality|default('', true) }}
+<b>Номер контракту:</b> {{ contract.contract_number|default('', true) }}
+<b>Конкурсний бал:</b> {{ contract.competitive_point|default('', true) }}
+<b>Дата:</b> {{ contract.date|default('', true) }}
 """)
 
 REGISTER_USER = environment.from_string("""
